7fe0f5d8e53548c27a23b0bdb7f2f7bce14221fa
miwasaki <4324018@ed.tus.ac.jp>
2026-06-23 20:33:08 +0900

    Learning about tkinter and move to another device

---
+++ fly-in/rendering/map_view.py
@@ -0,0 +1,6 @@
+from tkinter import ttk
+
+
+class MapView(ttk.Frame):
+    def __init__(self) -> None:
+        super().__init__()
--- fly-in/rendering/viewer_app.py
+++ fly-in/rendering/viewer_app.py
@@ -1,4 +1,21 @@
 import tkinter as tk
 
+from parse.map_parser import MapData
+
 root = tk.Tk()
 root.title("Fly-in")
+
+
+class ViewerApp(tk.Tk):
+    def __init__(self, map_data: MapData) -> None:
+        super().__init__()
+        self._map_data: MapData = map_data
+
+    def _build_layout(self) -> None:
+        pass
+
+    def _connect_callbacks(self) -> None:
+        pass
+
+    def _on_hover(self, target: Inspectable | None) -> None:
+        pass
